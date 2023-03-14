accc3a3a9c787754a808bf7ac2631258b18715f2
Riccardo Barbieri <riccardo_barbieri@outlook.it>
2023-03-14 09:30:36 +0100

    Auto-update of BarbieriRiccardoIss23 repository created 3 files:   	Appl1Sprint1/, Appl1Sprint2/, deleted 28 files:  	Appl1/.gitattributes, Appl1/.gitignore, Appl1/.gradle/buildOutputCleanup/cache.properties, Appl1/.gradle/buildOutputCleanup/outputFiles.bin, Appl1/.gradle/vcs-1/gc.properties, Appl1/.idea/.gitignore, Appl1/.idea/compiler.xml, Appl1/.idea/gradle.xml, Appl1/.idea/jarRepositories.xml, Appl1/.idea/misc.xml, Appl1/.idea/uiDesigner.xml, Appl1/.idea/vcs.xml, Appl1/app/build.gradle, Appl1/app/src/main/java/appl1/App.java, Appl1/app/src/test/java/appl1/AppTest.java, Appl1/application1/build.gradle, Appl1/application1/src/main/java/unibo/common/CollisionException.java, Appl1/application1/src/main/java/unibo/common/IVrobotMoves.java, Appl1/application1/src/main/java/unibo/common/VrobotMsgs.java, Appl1/application1/src/main/java/unibo/daevitare/unibo/http/FlatApplication1HTTPNoStop.java, Appl1/application1/src/main/java/unibo/model/RoomModel.java, Appl1/application1/src/main/java/unibo/supports/VrobotHLMovesHTTPApache.java, Appl1/application1/src/test/java/TestFlatApplication1HTTPNostop.java, Appl1/gradle/wrapper/gradle-wrapper.jar, Appl1/gradle/wrapper/gradle-wrapper.properties, Appl1/gradlew, Appl1/gradlew.bat, Appl1/settings.gradle modified 0 files:

---
+++ Appl1Sprint2/application1/src/main/java/unibo/common/CollisionException.java
@@ -0,0 +1,7 @@
+package unibo.common;
+
+public class CollisionException extends Exception {
+    public String getMessage() {
+        return "collision";
+    }
+}
---
+++ Appl1Sprint2/application1/src/main/java/unibo/model/RoomModel.java
@@ -0,0 +1,14 @@
+package unibo.model;
+
+public class RoomModel {
+    private int wallLeft, wallBottom, wallRight, wallTop;
+
+    public RoomModel(int wallLeft, int wallBottom, int wallRight, int wallTop) {
+        this.wallLeft = wallLeft;
+        this.wallBottom = wallBottom;
+        this.wallRight = wallRight;
+        this.wallTop = wallTop;
+    }
+
+
+}
